b5daa26a56ef214858dedb5100de787a39dee19d
Selen <parlarselen@gmail.com>
2018-10-08 13:30:05 +0300

    Added comments for documentation.

--- src/main/java/Classification/Performance/ClassificationPerformance.java
+++ src/main/java/Classification/Performance/ClassificationPerformance.java
@@ -1,20 +1,36 @@
 package Classification.Performance;
 
-public class ClassificationPerformance extends Performance{
+public class ClassificationPerformance extends Performance {
 
     private double accuracy;
 
-    public ClassificationPerformance(double accuracy){
+    /**
+     * A constructor that sets the accuracy and errorRate as 1 - accuracy via given accuracy.
+     *
+     * @param accuracy Double value input.
+     */
+    public ClassificationPerformance(double accuracy) {
         super(1 - accuracy);
         this.accuracy = accuracy;
     }
 
-    public ClassificationPerformance(double accuracy, double errorRate){
+    /**
+     * A constructor that sets the accuracy and errorRate via given input.
+     *
+     * @param accuracy  Double value input.
+     * @param errorRate Double value input.
+     */
+    public ClassificationPerformance(double accuracy, double errorRate) {
         super(errorRate);
         this.accuracy = accuracy;
     }
 
-    public double getAccuracy(){
+    /**
+     * Accessor for the accuracy.
+     *
+     * @return Accuracy value.
+     */
+    public double getAccuracy() {
         return accuracy;
     }
 
--- src/main/java/Classification/Performance/DetailedClassificationPerformance.java
+++ src/main/java/Classification/Performance/DetailedClassificationPerformance.java
@@ -3,12 +3,22 @@ package Classification.Performance;
 public class DetailedClassificationPerformance extends ClassificationPerformance {
     private ConfusionMatrix confusionMatrix;
 
-    public DetailedClassificationPerformance(ConfusionMatrix confusionMatrix){
+    /**
+     * A constructor that  sets the accuracy and errorRate as 1 - accuracy via given {@link ConfusionMatrix} and also sets the confusionMatrix.
+     *
+     * @param confusionMatrix {@link ConfusionMatrix} input.
+     */
+    public DetailedClassificationPerformance(ConfusionMatrix confusionMatrix) {
         super(confusionMatrix.getAccuracy());
         this.confusionMatrix = confusionMatrix;
     }
 
-    public ConfusionMatrix getConfusionMatrix(){
+    /**
+     * Accessor for the confusionMatrix.
+     *
+     * @return ConfusionMatrix.
+     */
+    public ConfusionMatrix getConfusionMatrix() {
         return confusionMatrix;
     }
 
