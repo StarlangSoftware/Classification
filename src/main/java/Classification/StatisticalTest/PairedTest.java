3a0cac8137a88372210535e2842b01470cbdf565
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-06-17 14:24:37 +0300

    Minor documentation update.

--- src/main/java/Classification/StatisticalTest/PairedTest.java
+++ src/main/java/Classification/StatisticalTest/PairedTest.java
@@ -7,9 +7,9 @@ public abstract class PairedTest {
 
     /**
      * Compares two classification algorithms based on their performances (accuracy or error rate). The method first
-     * checks the null hypothesis mu1 < mu2, if the test rejects this null hypothesis with alpha level of confidence, it
-     * decides mu1 > mu2. The algorithm then checks the null hypothesis mu1 > mu2, if the test rejects that null
-     * hypothesis with alpha level of confidence, if decides mu1 < mu2. If none of the two tests are rejected, it can not
+     * checks the null hypothesis mu1 is less than mu2, if the test rejects this null hypothesis with alpha level of confidence, it
+     * decides mu1 is larger than mu2. The algorithm then checks the null hypothesis mu1 is larger than mu2, if the test rejects that null
+     * hypothesis with alpha level of confidence, if decides mu1 is less than mu2. If none of the two tests are rejected, it can not
      * make a decision about the performances of algorithms.
      * @param classifier1 Performance (error rate or accuracy) results of the first classifier.
      * @param classifier2 Performance (error rate or accuracy) results of the second classifier.
--- src/main/java/Classification/StatisticalTest/StatisticalTestResult.java
+++ src/main/java/Classification/StatisticalTest/StatisticalTestResult.java
@@ -16,12 +16,12 @@ public class StatisticalTestResult {
 
     /**
      * Returns reject or failed to reject, depending on the alpha level and p value of the statistical test that checks
-     * one tailed null hypothesis such as mu1 < mu2. If p value is less than the alpha level, the test rejects the null
+     * one tailed null hypothesis such as mu1 is less than mu2. If p value is less than the alpha level, the test rejects the null
      * hypothesis. Otherwise, it fails to reject the null hypothesis.
      * @param alpha Alpha level of the test
      * @return If p value is less than the alpha level, the test rejects the null hypothesis. Otherwise, it fails to
      * reject the null hypothesis.
-     * @throws StatisticalTestNotApplicable If the null hypothesis is one tailed such as mu1 < mu2, and the statistical
+     * @throws StatisticalTestNotApplicable If the null hypothesis is one tailed such as mu1 is less than mu2, and the statistical
      * test's distribution is two tailed, then this test is not  applicable, therefore it throws
      * StatisticalTestNotApplicable.
      */
@@ -38,7 +38,7 @@ public class StatisticalTestResult {
 
     /**
      * Returns reject or failed to reject, depending on the alpha level and p value of the statistical test that checks
-     * one tailed null hypothesis such as mu1 < mu2 or two tailed null hypothesis such as mu1 = mu2. If the null
+     * one tailed null hypothesis such as mu1 is less than mu2 or two tailed null hypothesis such as mu1 = mu2. If the null
      * hypothesis is two tailed, and p value is less than the alpha level, the test rejects the null hypothesis.
      * Otherwise, it fails to reject the null hypothesis. If the null  hypothesis is one tailed, and p value is less
      * than alpha / 2 or p value is larger than 1 - alpha / 2, the test  rejects the null  hypothesis. Otherwise, it
